10dfb5221006466e7a0bff1b08dd25f4ac6f287a
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-10-18 15:17:09 +0100

    amedment to css

---
+++ index.js
@@ -0,0 +1,14 @@
+var articles = document.getElementsByClassName("article")
+
+var control = document.getElementById("control")
+
+var but = document.getElementById("but")
+
+but.addEventListener("click", function(){
+    alert(articles.length)
+    //var question = prompt("Do you love Javascript?")
+    // console.log({question})
+
+    //control.innerHTML = "<article class='article'>" + question + "</article>"
+
+})
